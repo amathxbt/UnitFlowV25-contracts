41a7051952fd5f8bc9d5f242dadcf04415d882cf
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2025-11-25 18:02:06 +0100

    Update TransferHelper.sol

--- contracts/periphery/contracts/libraries/TransferHelper.sol
+++ contracts/periphery/contracts/libraries/TransferHelper.sol
@@ -5,47 +5,63 @@ pragma solidity >=0.6.0;
 // helper methods for interacting with ERC20 tokens and sending ETH that do not consistently return true/false
 library TransferHelper {
     function safeApprove(
-        address token,
-        address to,
-        uint256 value
-    ) internal {
-        // bytes4(keccak256(bytes('approve(address,uint256)')));
-        (bool success, bytes memory data) = token.call(abi.encodeWithSelector(0x095ea7b3, to, value));
-        require(
-            success && (data.length == 0 || abi.decode(data, (bool))),
-            'TransferHelper::safeApprove: approve failed'
-        );
+    address token,
+    address spender,
+    uint256 value
+) internal {
+    (bool success, bytes memory data) = token.call(
+        abi.encodeWithSelector(0x095ea7b3, spender, value)
+    );
+
+    // First ensure the call itself succeeded
+    require(success, "TransferHelper::approve failed");
+
+    // If data is returned, it must decode to true
+    if (data.length > 0) {
+        require(abi.decode(data, (bool)), "TransferHelper::approve returned false");
     }
+}
 
     function safeTransfer(
-        address token,
-        address to,
-        uint256 value
-    ) internal {
-        // bytes4(keccak256(bytes('transfer(address,uint256)')));
-        (bool success, bytes memory data) = token.call(abi.encodeWithSelector(0xa9059cbb, to, value));
-        require(
-            success && (data.length == 0 || abi.decode(data, (bool))),
-            'TransferHelper::safeTransfer: transfer failed'
-        );
+    address token,
+    address to,
+    uint256 value
+) internal {
+    (bool success, bytes memory data) = token.call(
+        abi.encodeWithSelector(0xa9059cbb, to, value)
+    );
+
+    // First ensure the call itself succeeded
+    require(success, "TransferHelper::transfer failed");
+
+    // If data is returned, it must decode to true
+    if (data.length > 0) {
+        require(abi.decode(data, (bool)), "TransferHelper::transfer returned false");
     }
+}
 
     function safeTransferFrom(
-        address token,
-        address from,
-        address to,
-        uint256 value
-    ) internal {
-        // bytes4(keccak256(bytes('transferFrom(address,address,uint256)')));
-        (bool success, bytes memory data) = token.call(abi.encodeWithSelector(0x23b872dd, from, to, value));
-        require(
-            success && (data.length == 0 || abi.decode(data, (bool))),
-            'TransferHelper::transferFrom: transferFrom failed'
-        );
+    address token,
+    address from,
+    address to,
+    uint256 value
+) internal {
+    (bool success, bytes memory data) = token.call(
+        abi.encodeWithSelector(0x23b872dd, from, to, value)
+    );
+
+    // If the call itself failed, revert immediately
+    require(success, "TransferHelper::transferFrom failed");
+
+    // If data is returned, it must either be empty or decode to true
+    if (data.length > 0) {
+        // Some tokens return non-boolean data; check carefully
+        require(abi.decode(data, (bool)), "TransferHelper::transferFrom returned false");
     }
+}
 
     function safeTransferUSDC(address to, uint256 value) internal {
         (bool success, ) = to.call{value: value}(new bytes(0));
-        require(success, 'TransferHelper::safeTransferETH: ETH transfer failed');
+        require(success, 'TransferHelper::safeTransferUSDC: USDC transfer failed');
     }
 }
