9644d8e3f9c43ebda820f734babe30363a2de07f
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2025-11-25 10:50:17 +0100

    Update ArcFlowV25LiquidityRouter.sol

--- contracts/periphery/contracts/ArcFlowV25LiquidityRouter.sol
+++ contracts/periphery/contracts/ArcFlowV25LiquidityRouter.sol
@@ -1,6 +1,6 @@
 pragma solidity =0.6.6;
 
-import '@uniswap/v2-core/contracts/interfaces/IArcFlowV25Factory.sol';
+import '../core/contracts/interfaces/IArcFlowV25Factory.sol';
 import '@uniswap/lib/contracts/libraries/TransferHelper.sol';
 
 import './interfaces/IArcFlowV25LiquidityRouter.sol';
