c91d2070a4a57c7ad4b2f91d328164f9e204e61c
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2025-11-25 17:07:05 +0100

    Update UniswapV2Router02.sol

--- contracts/periphery/contracts/UniswapV2Router02.sol
+++ contracts/periphery/contracts/UniswapV2Router02.sol
@@ -1,7 +1,7 @@
 pragma solidity =0.6.6;
 
 import '../../core/contracts/interfaces/IArcFlowV25Factory.sol';
-import '@uniswap/lib/contracts/libraries/TransferHelper.sol';
+import './libraries/TransferHelper.sol';
 
 import './interfaces/IArcFlowV25Router02.sol';
 import './libraries/ArcFlowV25Library.sol';
