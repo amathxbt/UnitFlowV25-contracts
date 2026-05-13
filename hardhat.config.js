8131992086a7f308aa9fe3626dd40c339a53e241
UnitFlow Dev <dev@unitflow.finance>
2026-05-13 16:57:55 +0000

    Remove hardcoded private key; load from PRIVATE_KEY env var

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomicfoundation/hardhat-verify");
 
@@ -29,7 +30,7 @@ module.exports = {
     arcTestnet: {
       url: "https://rpc.testnet.arc.network",
       chainId: 5042002,
-      accounts: ["REDACTED_PRIVATE_KEY"],
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
       gasPrice: "auto",
     },
   },
